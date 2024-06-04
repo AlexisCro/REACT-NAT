ab8103bfffa713f30b6ad686f3ecaa34c622f9c9
AlexisCro <95683483+AlexisCro@users.noreply.github.com>
2024-06-04 09:51:29 +0200

    clean

--- myapp/app/_layout.js
+++ myapp/app/_layout.js
@@ -1,16 +1,6 @@
-import * as SplashScreen from 'expo-splash-screen';
 import { Stack } from 'expo-router';
-import { useEffect, useState } from 'react';
 
 export default function RootLayout() {
-  // load color
-  // load fonts
-
-  // display splashscreen
-  // hide splashscreen when everything is loaded
-
-  // Use hook useEffect (and useState if needed) to manage the state of the splashscreen
-
   return (
     <>
       <Stack>
